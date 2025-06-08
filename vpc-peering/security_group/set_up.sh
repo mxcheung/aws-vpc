fc2c8543b89bcf4ddaa69ac4cd2c485aee9bf7a6
mxcheung <mxcheung@users.noreply.github.com>
2025-06-08 18:34:48 +1000

    Update set_up.sh

--- vpc-peering/security_group/set_up.sh
+++ vpc-peering/security_group/set_up.sh
@@ -5,11 +5,21 @@
 WEB_VPC_ID=$(aws ec2 describe-vpcs --query "Vpcs[?Tags[?Key=='Name' && Value=='Web_VPC']].{VpcId:VpcId}" --output text)
 
 # Create a security group
-SG_ID=$(aws ec2 create-security-group \
+APP_SG_ID=$(aws ec2 create-security-group \
   --group-name WebWordPressSG \
   --description "Allow HTTP access to WordPress" \
   --vpc-id "$WEB_VPC_ID" \
   --query "GroupId" \
   --output text)
 
-echo $SG_ID
+echo $APP_SG_ID
+
+echo "Authorize inbound HTTP access"
+
+# Authorize inbound HTTP access
+INGRESS_OUTPUT=$(aws ec2 authorize-security-group-ingress \
+    --group-id $APP_SG_ID \
+    --protocol tcp \
+    --port 80 \
+    --cidr 0.0.0.0/0
+    
