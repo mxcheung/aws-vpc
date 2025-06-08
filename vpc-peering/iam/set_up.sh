e2d2e7e1b31c1e0cb688f41a6bda245f8c108a58
mxcheung <mxcheung@users.noreply.github.com>
2025-06-09 08:19:36 +1000

    Update set_up.sh

--- vpc-peering/iam/set_up.sh
+++ vpc-peering/iam/set_up.sh
@@ -8,15 +8,15 @@ POLICY_ARN="arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 # Check if IAM Role exists, create if not
 if ! aws iam get-role --role-name "$ROLE_NAME" >/dev/null 2>&1; then
   echo "Creating IAM role $ROLE_NAME..."
-  aws iam create-role --role-name "$ROLE_NAME" --assume-role-policy-document '{
+  IAM_ROLE_OUTPUT=$(aws iam create-role --role-name "$ROLE_NAME" --assume-role-policy-document '{
     "Version": "2012-10-17",
     "Statement": [{
       "Effect": "Allow",
       "Principal": { "Service": "ec2.amazonaws.com" },
       "Action": "sts:AssumeRole"
     }]
-  }'
-
+  }')
+  echo "IAM_ROLE_OUTPUT $IAM_ROLE_OUTPUT IAM role $ROLE_NAME..."
   echo "Attaching policy $POLICY_ARN to role..."
   aws iam attach-role-policy --role-name "$ROLE_NAME" --policy-arn "$POLICY_ARN"
 
