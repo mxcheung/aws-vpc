2b18609d24c76c9858ea425de90b9620bf833bd2
mxcheung <mxcheung@users.noreply.github.com>
2025-06-08 17:32:13 +1000

    Update set_up.sh

--- vpc-peering/set_up.sh
+++ vpc-peering/set_up.sh
@@ -2,3 +2,15 @@
 
 cd /home/cloudshell-user/aws-vpc/vpc-peering/user_credentials
 . ./set_up.sh
+
+cd /home/cloudshell-user/aws-vpc/vpc-peering/vpc
+. ./set_up.sh
+
+cd /home/cloudshell-user/aws-vpc/vpc-peering/subnets
+. ./set_up.sh
+
+cd /home/cloudshell-user/aws-vpc/vpc-peering/internet_gateway
+. ./set_up.sh
+
+cd /home/cloudshell-user/aws-vpc/vpc-peering/vpc_peering
+. ./set_up.sh
