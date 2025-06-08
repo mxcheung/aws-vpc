1288eb45bbde8ee22bf48b3c8c752f22e4d6c14c
mxcheung <mxcheung@users.noreply.github.com>
2025-06-08 17:13:26 +1000

    Update set_up.sh

--- vpc-peering/vpc/set_up.sh
+++ vpc-peering/vpc/set_up.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo "Creating VPC"
+echo "Creating VPC Web_VPC"
+
+aws ec2 create-vpc \
+  --cidr-block 192.168.0.0/16 \
+  --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=Web_VPC}]'
