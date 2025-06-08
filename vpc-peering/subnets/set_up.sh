e34d826592114c81d960c53bf4865bd35c15ef6c
mxcheung <mxcheung@users.noreply.github.com>
2025-06-08 17:15:53 +1000

    Update set_up.sh

--- vpc-peering/subnets/set_up.sh
+++ vpc-peering/subnets/set_up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Assign the VPC ID to a variable
-VPC_ID=$(aws ec2 describe-vpcs --query "Vpcs[?Tags[?Key=='Name' && Value=='Your Custom VPC']].{VpcId:VpcId}" --output text)
+VPC_ID=$(aws ec2 describe-vpcs --query "Vpcs[?Tags[?Key=='Name' && Value=='Web_VPC']].{VpcId:VpcId}" --output text)
 
 echo $VPC_ID
 
