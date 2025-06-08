65755eaa05d80030b85e5a0a87fd024240a0f9f2
mxcheung <mxcheung@users.noreply.github.com>
2025-06-08 18:23:01 +1000

    Update set_up.sh

--- vpc-peering/ec2/set_up.sh
+++ vpc-peering/ec2/set_up.sh
@@ -32,32 +32,71 @@ echo "Subnet ID: $SUBNET_ID"
 # Define User Data (WordPress install bootstrap script)
 read -r -d '' USER_DATA <<'EOF'
 #!/bin/bash
-sudo apt update
-sudo apt install apache2 php libapache2-mod-php php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip unzip -y
-sudo ufw allow in "Apache"
-sudo a2enmod rewrite
-systemctl restart apache2
-cd /tmp/ && wget https://wordpress.org/latest.zip
-unzip latest.zip -d /var/www
-chown -R www-data:www-data /var/www/wordpress/
-mv /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
-cd /var/www/wordpress/
-perl -pi -e "s/database_name_here/wordpress/g" wp-config.php
-perl -pi -e "s/username_here/wordpress/g" wp-config.php
-perl -pi -e "s/password_here/wordpress/g" wp-config.php
+
+LOGFILE=/var/log/userdata.log
+exec > >(tee -a $LOGFILE) 2>&1
+
+echo "=== User Data Script Started at $(date) ==="
+
+set +e  # Don't exit on error
+
+echo "Updating packages..."
+sudo apt update || true
+
+echo "Installing WordPress dependencies..."
+sudo apt install -y apache2 php libapache2-mod-php php-mysql php-curl php-gd php-mbstring \
+  php-xml php-xmlrpc php-soap php-intl php-zip unzip || true
+
+echo "Allowing Apache through UFW (if enabled)..."
+sudo ufw allow in "Apache" || true
+
+echo "Enabling Apache rewrite module..."
+sudo a2enmod rewrite || true
+
+echo "Restarting Apache..."
+systemctl restart apache2 || true
+
+echo "Downloading WordPress..."
+cd /tmp/ && wget https://wordpress.org/latest.zip || true
+
+echo "Unzipping WordPress..."
+unzip -o latest.zip -d /var/www || true
+
+echo "Setting permissions for WordPress..."
+chown -R www-data:www-data /var/www/wordpress/ || true
+
+echo "Setting up wp-config.php..."
+mv /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php || true
+
+cd /var/www/wordpress || true
+
+echo "Configuring database settings..."
+perl -pi -e "s/database_name_here/wordpress/g" wp-config.php || true
+perl -pi -e "s/username_here/wordpress/g" wp-config.php || true
+perl -pi -e "s/password_here/wordpress/g" wp-config.php || true
+
+echo "Inserting salts into wp-config.php..."
 perl -i -pe'
 BEGIN {
 @chars = ("a" .. "z", "A" .. "Z", 0 .. 9);
-push @chars, split //, "!@#$%^&*()-_ []{}<>~\`+=,.;:/?|";
+push @chars, split //, "!@#$%^&*()-_ []{}<>~`+=,.;:/?|";
 sub salt { join "", map $chars[ rand @chars ], 1 .. 64 }
 }
 s/put your unique phrase here/salt()/ge
-' wp-config.php
-wget https://raw.githubusercontent.com/ACloudGuru-Resources/course-aws-certified-solutions-architect-associate/main/lab/5/000-default.conf
-mkdir wp-content/uploads
-chmod 775 wp-content/uploads
-mv 000-default.conf /etc/apache2/sites-enabled/
-systemctl restart apache2
+' wp-config.php || true
+
+echo "Downloading Apache config override..."
+wget -O /etc/apache2/sites-enabled/000-default.conf \
+  https://raw.githubusercontent.com/ACloudGuru-Resources/course-aws-certified-solutions-architect-associate/main/lab/5/000-default.conf || true
+
+echo "Creating uploads directory..."
+mkdir -p wp-content/uploads || true
+chmod 775 wp-content/uploads || true
+
+echo "Restarting Apache again..."
+systemctl restart apache2 || true
+
+echo "=== User Data Script Completed at $(date) ==="
 EOF
 
 # Create a security group
