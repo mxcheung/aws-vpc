bf563c5cf53424485bf2315e3ff4bc9224b90225
mxcheung <mxcheung@users.noreply.github.com>
2025-06-09 08:16:56 +1000

    Update set_up.sh

--- vpc-peering/set_up.sh
+++ vpc-peering/set_up.sh
@@ -18,6 +18,9 @@ cd /home/cloudshell-user/aws-vpc/vpc-peering/security_group
 cd /home/cloudshell-user/aws-vpc/vpc-peering/vpc_peering
 . ./set_up.sh
 
+cd /home/cloudshell-user/aws-vpc/vpc-peering/iam
+. ./set_up.sh
+
 cd /home/cloudshell-user/aws-vpc/vpc-peering/ec2
 . ./set_up.sh
 
