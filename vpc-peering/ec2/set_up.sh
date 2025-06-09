dbe597d61bee8b1d03095169bdbd96a92ec82daf
mxcheung <mxcheung@users.noreply.github.com>
2025-06-09 11:55:54 +1000

    Update set_up.sh

--- vpc-peering/ec2/set_up.sh
+++ vpc-peering/ec2/set_up.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 IAM_INSTANCE_PROFILE_NAME="EC2SSMRole"  # must match role created above
 
 sudo cp /home/cloudshell-user/aws-vpc/vpc-peering/keypair/MyKeyPair.pem MyKeyPair.pem 
-sudo chmod 444 MyKeyPair.pem 
+sudo chmod 400 MyKeyPair.pem 
 KEY_NAME="MyKeyPair"
 
 # Get VPC ID
