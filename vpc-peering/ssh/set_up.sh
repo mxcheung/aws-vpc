a7f7d22c3cac01310413e2035cc084a51b39dae4
mxcheung <mxcheung@users.noreply.github.com>
2025-06-09 11:13:51 +1000

    Update set_up.sh

--- vpc-peering/ssh/set_up.sh
+++ vpc-peering/ssh/set_up.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-aws ec2 describe-instances \
+
+INSTANCE_NAME="WordPressInstance"
+INSTANCE_ID=$(aws ec2 describe-instances \
+  --filters "Name=tag:Name,Values=$INSTANCE_NAME" \
+  --query "Reservations[].Instances[].InstanceId" \
+  --output text)
+
+echo "EC2 WordPressInstance instance ID: $INSTANCE_ID"
+
+EC2_PUBLIC_IP=$(aws ec2 describe-instances \
   --instance-ids "$INSTANCE_ID" \
   --query "Reservations[0].Instances[0].PublicIpAddress" \
-  --output text
+  --output text_
+
+echo "EC2 PUBLIC IP: $EC2_PUBLIC_IP"
+
+
+FILE_PATH="/var/www/wordpress/wp-config.php"
+
+ssh -i MyKeyPair.pem ubuntu@<PUBLIC_IP>     # Ub
